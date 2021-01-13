36f7bfa51c2bb6ac0991a3f85fac8a4c4e67972e
zhangxianbing <zhangxianbing09@163.com>
2021-01-13 15:26:29 +0800

    feat: bump version v1.0.2

--- jsonpath/__init__.py
+++ jsonpath/__init__.py
@@ -2,10 +2,10 @@
 Author       : zhangxianbing
 Date         : 2020-12-27 09:22:14
 LastEditors  : zhangxianbing
-LastEditTime : 2021-01-13 13:29:48
+LastEditTime : 2021-01-13 15:25:25
 Description  : JSONPath
 """
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __author__ = "zhangxianbing"
 
 import json

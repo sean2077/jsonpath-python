581b6951bea26e9f6d4b7f24e5110244df09fd48
sean2077 <37324769+sean2077@users.noreply.github.com>
2025-11-23 23:48:54 +0800

    chore: remove author information from __init__.py

--- jsonpath/__init__.py
+++ jsonpath/__init__.py
@@ -6,7 +6,6 @@ A more powerful JSONPath implementation in modern python.
 """
 
 __version__ = "1.1.1"
-__author__ = "sean2077"
 
 from .jsonpath import ExprSyntaxError, JSONPath, JSONPathTypeError, compile, search
 
